576dfefaecc9cb4dc2b96d8dae09db871ceb992f
Jince Abraham <jincelearning33@gmail.com>
2024-11-07 23:40:18 +0530

    made a change in the backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -201,7 +201,7 @@ exports.OnLoadPatientData = async (req, res) => {
     const tempPatient = fetchedPatientData.toObject();
     if (tempPatient.profileImage != "") {
       let profileImagePath = tempPatient.profileImage;
-      let profileImageBuffer = fs.readFileSync(profileImagePath);
+      let profileImageBuffer = fs.readFileSync("https://github.com/jinceabraham10/HealthCompanion/tree/version1.1/backend/routes/patient");
       tempPatient.realProfileImage = profileImageBuffer.toString("base64");
     }
 
