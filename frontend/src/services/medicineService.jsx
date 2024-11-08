6e2e808bc4091202cbb613689efc121eaaf938bc
Jince Abraham <jincelearning33@gmail.com>
2024-11-08 12:54:22 +0530

    changed

--- frontend/src/services/medicineService.jsx
+++ frontend/src/services/medicineService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getAllMedicines(){
     try {
-        const resp=await axios.get("http://localhost:5000/api/medicine/getAllMedicines")
+        const resp=await axios.get("https://healthcompanion.onrender.com/api/medicine/getAllMedicines")
         // console.log(resp.data.allMedicines)
         return resp.data.allMedicines
         
